cfc6ea8680646ffc315c1e274e63f93bd95054fb
Juan Camilo Serna Trejos <149125041+JuanCaSerTre@users.noreply.github.com>
2024-10-21 17:51:03 +1000

    Project Structure.
    Creation of project structure (components) most of it for frontend side.

---
+++ frontend/src/app/MainComponents/recommendation-list/recommendation-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './recommendation-list.component.html',
+  styleUrl: './recommendation-list.component.css'
+})
+export class RecommendationListComponent {
+
+}
